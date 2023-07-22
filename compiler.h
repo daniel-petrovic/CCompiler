6fad72fef5bbb1684557d9395b32f6053ed8ded4
Daniel Petrovic <daniel-dev@hotmail.de>
2023-07-22 15:02:36 +0300

    Add number type

--- compiler.h
+++ compiler.h
@@ -74,6 +74,13 @@ enum
     TOKEN_TYPE_NEWLINE
 };
 
+enum {
+    NUMBER_TYPE_NORMAL,
+    NUMBER_TYPE_LONG,
+    NUMBER_TYPE_FLOAT,
+    NUMBER_TYPE_DOUBLE
+};
+
 struct token
 {
 
@@ -90,6 +97,10 @@ struct token
         unsigned long long llnum;
         void *any;
     };
+
+    struct token_number {
+        int type;
+    } num;
 
     // True if there is whitespace between the token and the next token
     // i.e * a for operator token * would mean whitespace would be set for token "a"
